aad25cedc9446841303ce21dd1f0a876ef4506a1
Ganesh-346 <saiganeshbhimavarapu88@gmail.com>
2025-02-10 11:41:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
 
-import StudentDashboard from './components/User/StudentDashboard.jsx'
-import AdminDashboard from './components/Admin/AdminDashboard';
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/admin/*" element={<AdminDashboard />} />
-        {/* Other routes */}
-      </Routes>
+ <h1>hello</h1>
     </>
   );
 }
